0de1bab632f91282bd317a159ca0387f8d9735fb
Marachinos <stallnysater@gmail.com>
2025-10-26 16:20:45 +0100

    changes

--- js/helios.js
+++ js/helios.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const newsBox = document.createElement("div");
     newsBox.classList.add("news-box");
     newsBox.innerHTML = `
-      <h3>Senaste nytt</h3>
+      <h2>Senaste nytt</h2>
       <p>Helios storebror Hackels Lumiere VANN i sin 10:e start! 12.000 kr och nytt rekord på 1.17,9</p>
       
     `;
